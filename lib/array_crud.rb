7563190bba26c00a7349ce35edfacd7fe5fa4da2
John Grubbs <johnmlowery@gmail.com>
2018-08-17 23:49:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   anArray = ["mice", 'b', 'f', 'd']
-  anArray.push("c")
+  anArray.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
