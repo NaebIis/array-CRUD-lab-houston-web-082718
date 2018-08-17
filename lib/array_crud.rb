e0b59aeb781c9080bc2b64615b02aedc12423690
John Grubbs <johnmlowery@gmail.com>
2018-08-17 23:52:28 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  anArray = ["mice", 'b', 'f', 'arrays!']
+  anArray = ["wow", 'b', 'f', 'arrays!']
   anArray.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  anArray = ["wow", 'b', 'f', 'arrays!']
 end
 
 def retrieve_element_from_index(array, index_number)
