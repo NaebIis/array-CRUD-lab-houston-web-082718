209b5fef543dcfed9f8ec100f9542fdd1b8cc731
John Grubbs <johnmlowery@gmail.com>
2018-08-17 23:50:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  anArray = ["mice", 'b', 'f', 'd']
+  anArray.unshift("first")
 end
 
 def remove_element_from_end_of_array(array)
