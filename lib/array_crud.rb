d639f39c7a47892dd953ac92bca1c0560ad18eaf
John Grubbs <johnmlowery@gmail.com>
2018-08-17 23:59:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  anArray = ["wow", 'b', 'f', 'arrays!']
   anArray[2]
 end
 
 def retrieve_first_element_from_array(array)
+  anArray = ["wow", 'b', 'f', 'arrays!']
   anArray.first
 end
 
 def retrieve_last_element_from_array(array)
+  anArray = ["wow", 'b', 'f', 'arrays!']
   anArray.last
 end
