c2bea6574ff68ea19a47708ede6917d4b397c979
John Grubbs <johnmlowery@gmail.com>
2018-08-18 00:00:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  anArray = ["wow", 'b', 'f', 'arrays!']
+  anArray = ["wow", 'b', 'am', 'arrays!']
   anArray.last
 end
